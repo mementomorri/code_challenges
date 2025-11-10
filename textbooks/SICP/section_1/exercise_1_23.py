dd486fb8025e64e3f7afd0d8856f689ae61ef9ed
a.karsten <mementomorimore@gmail.com>
2025-11-10 05:45:52 -0300

    add exercise 1.23

--- textbooks/SICP/section_1/exercise_1_23.py
+++ textbooks/SICP/section_1/exercise_1_23.py
@@ -1,11 +1,12 @@
-import math
-import random
 import time
-# from functools import wraps
 
 
 def smallest_divisor(n: int):
     return find_devisor(n, 2)
+
+
+def next_d(d: int) -> int:
+    return d + 1 if d == 2 else d + 2
 
 
 def find_devisor(n: int, d: int):
@@ -14,7 +15,7 @@ def find_devisor(n: int, d: int):
     elif n % d == 0:
         return d
     else:
-        return find_devisor(n, d + 1)
+        return find_devisor(n, next_d(d))
 
 
 def is_prime(n):
@@ -28,52 +29,13 @@ def timed_prime_test(n: int, show_log=None) -> bool:
 
 
 def start_prime_test(n: int, start_time: float) -> bool:
-    return report_prime(time.time() - start_time) if fast_is_prime(n, 3) else False
+    return report_prime(time.time() - start_time) if is_prime(n) else False
 
 
 def report_prime(elapsed_time: float) -> bool:
     print(" *** ")
     print(f"Time elapsed: {elapsed_time} seconds")
     return True
-
-
-# def track_elapsed_time(func):
-#     @wraps(func)
-#     def wrapper(*args, **kwargs):
-#         start_time = time.perf_counter()  # Start the timer
-#         result = func(*args, **kwargs)  # Call the function
-#         end_time = time.perf_counter()  # End the timer
-#         elapsed_time = end_time - start_time  # Calculate elapsed time
-#         print(f"Elapsed time for '{func.__name__}': {elapsed_time:.6f} seconds")
-#         return result
-#
-#     return wrapper
-
-
-# Probabalistic approach with O(log n)
-def expmod(b: int, exp: int, m: int):
-    if exp == 0:
-        return 1
-    elif exp % 2 == 0:
-        return expmod(b, exp // 2, m) ** 2
-    else:
-        return (b * expmod(b, exp - 1, m)) % m
-
-
-def fermat_test(n: int):
-    def try_it(a: int):
-        return expmod(a, n, n) == a
-
-    return try_it(1 + math.floor(random.random() * (n - 1)))
-
-
-def fast_is_prime(n: int, times: int):
-    if times == 0:
-        return True
-    elif fermat_test(n):
-        return fast_is_prime(n, times - 1)
-    else:
-        return False
 
 
 def search_for_primes(search_rage: tuple[int, int], show_log=None) -> tuple[int]:
@@ -95,5 +57,6 @@ if __name__ == "__main__":
     print("search_for_primes(10000, 100000):", search_for_primes((10000, 100000)))
     print("search_for_primes(100000, 1000000):", search_for_primes((100000, 1000000)))
     print(
-        "search_for_primes(1000000, 10000000):", search_for_primes((1000000, 10000000))
+        "search_for_primes(1000000, 10000000):",
+        search_for_primes((1000000, 10000000)),
     )
