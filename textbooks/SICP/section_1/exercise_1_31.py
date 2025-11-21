5328d72486ac06f7dc667ef8363283dc4861760c
mementomori <mementomorimore@gmail.com>
2025-11-21 07:40:26 -0300

    modify exercise 1.31

--- textbooks/SICP/section_1/exercise_1_31.py
+++ textbooks/SICP/section_1/exercise_1_31.py
@@ -3,7 +3,7 @@ import math
 # import trace
 
 
-def product(f: Callable, a: int | float, b: int | float) -> int | float:
+def product(f: Callable, a: int, b: int | float, c=None) -> int | float:
     """function that returns the product of the values of a function at points over a given range."""
 
     # def _iter(a: int | float, result: int | float):
@@ -11,11 +11,13 @@ def product(f: Callable, a: int | float, b: int | float) -> int | float:
     #
     # return _iter(a, 1)
 
-    result = 1
+    result = a
     stop_b = math.floor(b + 1)
+    if c is None:
+        c = 1
 
-    for a in range(1, stop_b):
-        result *= f(a)
+    for n in range(a, stop_b, c):
+        result *= f(n)
 
     return result
 
@@ -24,5 +26,15 @@ def factorial(n):
     return product(lambda x: x, 1, n)
 
 
+def product_pi(n: int) -> float:
+    """Calculate approximation to Pi using Wallis method."""
+
+    def multiply_by_self(n):
+        return n + n
+
+    return (product(lambda x: x + 2, 2, n, 2)) / product(multiply_by_self, 3, n, 2)
+
+
 if __name__ == "__main__":
-    print(f"testing factorial: {factorial(10)}")
+    print(f"testing factorial of 10: {factorial(10)}")
+    # print(f"testing product_pi: {product_pi(10)}")
